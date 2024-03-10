d4e76c644f7b202aca0b8ae68e59d68fcff8e35b
Jens Axboe <axboe@kernel.dk>
2024-03-10 14:50:06 -0600

    sched/core: change rq->nr_iowait type to atomic_long_t on 64-bit
    In preparation for storing two separate iowait states in there, bump the
    size from a 32-bit to a 64-bit size, for 64-bit kernels.
    
    Note that on 32-bit, the number of tasks are limited to 0x8000, which
    fits just fine in even half of the existiing 32-bit atomic_t. For 64-bit,
    no such limit exists, hence play it safe and make it a 64-bit atomic.
    
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -3543,17 +3543,29 @@ static inline bool rq_has_pinned_tasks(struct rq *rq)
 
 static void task_iowait_inc(struct task_struct *p)
 {
+#ifdef CONFIG_64BIT
+	atomic_long_inc(&task_rq(p)->nr_iowait);
+#else
 	atomic_inc(&task_rq(p)->nr_iowait);
+#endif
 }
 
 static void task_iowait_dec(struct task_struct *p)
 {
+#ifdef CONFIG_64BIT
+	atomic_long_dec(&task_rq(p)->nr_iowait);
+#else
 	atomic_dec(&task_rq(p)->nr_iowait);
+#endif
 }
 
 int rq_iowait(struct rq *rq)
 {
+#ifdef CONFIG_64BIT
+	return atomic_long_read(&rq->nr_iowait);
+#else
 	return atomic_read(&rq->nr_iowait);
+#endif
 }
 
 static void
@@ -8372,7 +8384,7 @@ void __init sched_init(void)
 #endif
 #endif /* CONFIG_SMP */
 		hrtick_rq_init(rq);
-		atomic_set(&rq->nr_iowait, 0);
+		atomic_long_set(&rq->nr_iowait, 0);
 		fair_server_init(rq);
 
 #ifdef CONFIG_SCHED_CORE
