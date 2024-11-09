adb32390b46465ddb96a4f1c2f71acf6dab3be5b
Jens Axboe <axboe@kernel.dk>
2024-11-09 10:03:01 -0700

    mm: add FGP_UNCACHED folio creation flag
    Callers can pass this in for uncached folio creation, in which case if
    a folio is newly created it gets marked as uncached. If a folio exists
    for this index and lookup succeeds, then it will not get marked as
    uncached. If an !uncached lookup finds a cached folio, clear the flag.
    For that case, there are competeting uncached and cached users of the
    folio, and it should not get pruned.
    
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

--- mm/filemap.c
+++ mm/filemap.c
@@ -1985,6 +1985,8 @@ no_page:
 			/* Init accessed so avoid atomic mark_page_accessed later */
 			if (fgp_flags & FGP_ACCESSED)
 				__folio_set_referenced(folio);
+			if (fgp_flags & FGP_UNCACHED)
+				__folio_set_uncached(folio);
 
 			err = filemap_add_folio(mapping, folio, index, gfp);
 			if (!err)
@@ -2007,6 +2009,9 @@ no_page:
 
 	if (!folio)
 		return ERR_PTR(-ENOENT);
+	/* not an uncached lookup, clear uncached if set */
+	if (folio_test_uncached(folio) && !(fgp_flags & FGP_UNCACHED))
+		folio_clear_uncached(folio);
 	return folio;
 }
 EXPORT_SYMBOL(__filemap_get_folio);
