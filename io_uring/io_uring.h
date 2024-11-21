ff401eebd1294c2396bf290d6f1edfe9e7906f91
Jens Axboe <axboe@kernel.dk>
2024-11-21 08:27:10 -0700

    io_uring: make task_work pending check dependent on ring type
    There's no need to check for generic task_work for DEFER_TASKRUN, if we
    have local task_work pending. This avoids dipping into the huge
    task_struct, if we have normal task_work pending.
    
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

--- io_uring/io_uring.h
+++ io_uring/io_uring.h
@@ -354,7 +354,9 @@ static inline bool io_local_work_pending(struct io_ring_ctx *ctx)
 
 static inline bool io_task_work_pending(struct io_ring_ctx *ctx)
 {
-	return task_work_pending(current) || io_local_work_pending(ctx);
+	if (ctx->flags & IORING_SETUP_DEFER_TASKRUN && io_local_work_pending(ctx))
+		return true;
+	return task_work_pending(current);
 }
 
 static inline void io_tw_lock(struct io_ring_ctx *ctx, struct io_tw_state *ts)
