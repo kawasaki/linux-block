657632cbc1a351d1aebeabc53fbe22b12248a3b8
Thomas Haschka <thomas.haschka@tuwien.ac.at>
2025-02-21 16:41:02 +0100

    Patch and bugfix in block/blk-mq.c to regain stability when using various SD Cards and Card Readers.
    Bug Fix: block: Improve stability of SD cards in Microsoft Surface GO 2 and
         	 	possibly other devices.
    
    The commit 65a558f66c308
         block: Improve performance for BLK_MQ_F_BLOCKING drivers
    
    basically made the use of SD cards in my Microsoft Surface GO 2 impossible.
    The cards do stop functioning after about 15 minutes. Mostly at io intensive
    tasks.
    
    As outlined in https://bugzilla.kernel.org/show_bug.cgi?id=218821
    i bisected the problem that yielded an unstable operation of the cardreader
    on my Surface GO 2.
    I successfully reversed the commit 65a558f66c308 in 6.12.16 using
    the attached patch. As I suppose the bug introduced with this commit might
    hit other users of sd-cards in similar hardware I suggest this commit shall
    be reversed, even if the improved performance might be gone.
    
    All the best,
    Thomas Haschka
    
    diff '--color=auto' -urpN a/block/blk-mq.c b/block/blk-mq.c

--- block/blk-mq.c
+++ block/blk-mq.c
@@ -1425,7 +1425,7 @@ void blk_execute_rq_nowait(struct request *rq, bool at_head)
 	}
 
 	blk_mq_insert_request(rq, at_head ? BLK_MQ_INSERT_AT_HEAD : 0);
-	blk_mq_run_hw_queue(hctx, hctx->flags & BLK_MQ_F_BLOCKING);
+	blk_mq_run_hw_queue(hctx, false);
 }
 EXPORT_SYMBOL_GPL(blk_execute_rq_nowait);
 
@@ -2326,8 +2326,6 @@ void blk_mq_run_hw_queue(struct blk_mq_hw_ctx *hctx, bool async)
 	 */
 	WARN_ON_ONCE(!async && in_interrupt());
 
-	might_sleep_if(!async && hctx->flags & BLK_MQ_F_BLOCKING);
-
 	need_run = blk_mq_hw_queue_need_run(hctx);
 	if (!need_run) {
 		unsigned long flags;
@@ -2346,7 +2344,8 @@ void blk_mq_run_hw_queue(struct blk_mq_hw_ctx *hctx, bool async)
 			return;
 	}
 
-	if (async || !cpumask_test_cpu(raw_smp_processor_id(), hctx->cpumask)) {
+	if (async || (hctx->flags & BLK_MQ_F_BLOCKING) ||
+	    !cpumask_test_cpu(raw_smp_processor_id(), hctx->cpumask)) {
 		blk_mq_delay_run_hw_queue(hctx, 0);
 		return;
 	}
@@ -2481,7 +2480,7 @@ void blk_mq_start_hw_queue(struct blk_mq_hw_ctx *hctx)
 {
 	clear_bit(BLK_MQ_S_STOPPED, &hctx->state);
 
-	blk_mq_run_hw_queue(hctx, hctx->flags & BLK_MQ_F_BLOCKING);
+	blk_mq_run_hw_queue(hctx, false);
 }
 EXPORT_SYMBOL(blk_mq_start_hw_queue);
 
@@ -2517,8 +2516,7 @@ void blk_mq_start_stopped_hw_queues(struct request_queue *q, bool async)
 	unsigned long i;
 
 	queue_for_each_hw_ctx(q, hctx, i)
-		blk_mq_start_stopped_hw_queue(hctx, async ||
-					(hctx->flags & BLK_MQ_F_BLOCKING));
+		blk_mq_start_stopped_hw_queue(hctx, async);
 }
 EXPORT_SYMBOL(blk_mq_start_stopped_hw_queues);
 
@@ -2576,8 +2574,6 @@ static void blk_mq_insert_requests(struct blk_mq_hw_ctx *hctx,
 	list_for_each_entry(rq, list, queuelist) {
 		BUG_ON(rq->mq_ctx != ctx);
 		trace_block_rq_insert(rq);
-		if (rq->cmd_flags & REQ_NOWAIT)
-			run_queue_async = true;
 	}
 
 	spin_lock(&ctx->lock);
@@ -2744,7 +2740,7 @@ static void blk_mq_try_issue_directly(struct blk_mq_hw_ctx *hctx,
 
 	if ((rq->rq_flags & RQF_USE_SCHED) || !blk_mq_get_budget_and_tag(rq)) {
 		blk_mq_insert_request(rq, 0);
-		blk_mq_run_hw_queue(hctx, rq->cmd_flags & REQ_NOWAIT);
+		blk_mq_run_hw_queue(hctx, false);
 		return;
 	}
 
